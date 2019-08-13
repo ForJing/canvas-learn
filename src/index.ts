f961a36df6123e8bbacb9b9e1523f567585a6bde
forjing <xiaodoubao1994@gmail.com>
2019-08-13 18:27:52 +0800

    cavas 画正弦图

--- src/index.ts
+++ src/index.ts
@@ -6,26 +6,24 @@ import Ball from "./ball";
 window.onload = function() {
   const canvas = document.getElementById("canvas") as HTMLCanvasElement;
   const context = canvas.getContext("2d");
-
-  const ball = new Ball();
-  let angleX = 0;
-  let angleY = 0;
+  let angle = 0;
   const range = 50;
-  const centerX = canvas.width / 2;
   const centerY = canvas.height / 2;
-  const xspeed = 0.07;
-  const yspeed = 0.11;
+  const xspeed = 1;
+  const yspeed = 0.05;
+  let xpos = 0;
+  let ypos = centerY;
 
   (function drawFrame() {
     requestAnimationFrame(drawFrame);
-    context.clearRect(0, 0, canvas.width, canvas.height);
 
-    ball.x = centerX + Math.sin(angleX) * range;
-    ball.y = centerY + Math.sin(angleY) * range;
+    context.beginPath();
+    context.moveTo(xpos, ypos);
 
-    angleX += xspeed;
-    angleY += yspeed;
-
-    ball.draw(context);
+    xpos += xspeed;
+    angle += yspeed;
+    ypos = centerY - Math.sin(angle) * range;
+    context.lineTo(xpos, ypos);
+    context.stroke();
   })();
 };
