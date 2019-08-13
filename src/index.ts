e9007b190913f29d88d11a704bcea22ff6e718ee
forjing <xiaodoubao1994@gmail.com>
2019-08-13 18:01:24 +0800

    random

--- src/index.ts
+++ src/index.ts
@@ -8,20 +8,24 @@ window.onload = function() {
   const context = canvas.getContext("2d");
 
   const ball = new Ball();
-  let angle = 0;
-  const centerScale = 1;
-  const range = 0.5;
-  const speed = 0.05;
-
-  ball.x = canvas.width / 2;
-  ball.y = canvas.height / 2;
+  let angleX = 0;
+  let angleY = 0;
+  const range = 50;
+  const centerX = canvas.width / 2;
+  const centerY = canvas.height / 2;
+  const xspeed = 0.07;
+  const yspeed = 0.11;
 
   (function drawFrame() {
     requestAnimationFrame(drawFrame);
     context.clearRect(0, 0, canvas.width, canvas.height);
 
-    ball.scaleX = ball.scaleY = centerScale + Math.sin(angle) * range;
-    angle += speed;
+    ball.x = centerX + Math.sin(angleX) * range;
+    ball.y = centerY + Math.sin(angleY) * range;
+
+    angleX += xspeed;
+    angleY += yspeed;
+
     ball.draw(context);
   })();
 };
