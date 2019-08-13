3b666a15b56cbc8c4a7c5ed6d82db377c1a7e311
forjing <xiaodoubao1994@gmail.com>
2019-08-13 17:34:05 +0800

    add ball

--- src/index.ts
+++ src/index.ts
@@ -6,20 +6,19 @@ import Ball from "./ball";
 window.onload = function() {
   const canvas = document.getElementById("canvas") as HTMLCanvasElement;
   const context = canvas.getContext("2d");
-  const mouse = captureMouse(canvas);
-  const arrow = new Arrow();
 
-  arrow.x = canvas.width / 2;
-  arrow.y = canvas.height / 2;
+  const ball = new Ball();
+  let angle = 0;
+
+  ball.x = canvas.width / 2;
+  ball.y = canvas.height / 2;
 
   (function drawFrame() {
     requestAnimationFrame(drawFrame);
     context.clearRect(0, 0, canvas.width, canvas.height);
 
-    const dx = mouse.x - arrow.x;
-    const dy = mouse.y - arrow.y;
-
-    arrow.rotation = Math.atan2(dy, dx);
-    arrow.draw(context);
+    ball.y = canvas.height / 2 + Math.sin(angle) * 50;
+    angle += 0.1;
+    ball.draw(context);
   })();
 };
