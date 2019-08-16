20cf0342b21b258b83dd99c332bd4a32d3afa6eb
forjing <xiaodoubao1994@gmail.com>
2019-08-16 14:14:06 +0800

    draw image

--- src/index.ts
+++ src/index.ts
@@ -7,34 +7,9 @@ window.onload = function() {
   const canvas = document.getElementById("canvas") as HTMLCanvasElement;
   const context = canvas.getContext("2d");
 
-  // const gradient = context.createLinearGradient(
-  //   0,
-  //   0,
-  //   canvas.width,
-  //   canvas.height
-  // );
-  // gradient.addColorStop(0, "#f00");
-  // gradient.addColorStop(1, "rgba(255, 0, 0, 0.1)");
-
-  // context.fillStyle = gradient;
-
-  // context.fillRect(0, 0, canvas.width, canvas.height);
-
-  const c1 = { x: 150, y: 150, radius: 30 };
-  const c2 = { x: 150, y: 150, radius: 60 };
-  const gradient = context.createRadialGradient(
-    c1.x,
-    c1.y,
-    c1.radius,
-    c2.x,
-    c2.y,
-    c2.radius
-  );
-  gradient.addColorStop(0.5, "blue");
-  gradient.addColorStop(1, "green");
-  context.fillStyle = gradient;
-  context.arc(150, 150, 60, 0, Math.PI * 2);
-  context.fill();
-  context.arc(150, 150, 30, 0, Math.PI * 2)
-  context.stroke()
+  const img = new Image();
+  img.src = require("./1.jpg");
+  img.onload = function() {
+    context.drawImage(img, 0, 0, 100, 100, 0, 0, 50, 100);
+  };
 };
