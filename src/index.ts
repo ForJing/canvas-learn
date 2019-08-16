f8b802a9d39a7524a1a25e43224f963aaf486fa8
forjing <xiaodoubao1994@gmail.com>
2019-08-16 13:18:05 +0800

    curve

--- src/index.ts
+++ src/index.ts
@@ -7,20 +7,21 @@ window.onload = function() {
   const canvas = document.getElementById("canvas") as HTMLCanvasElement;
   const context = canvas.getContext("2d");
 
+  const x0 = 100;
+  const y0 = 200;
+  const x2 = 300;
+  const y2 = 200;
+
   const mouse = captureMouse(canvas);
 
-  function onMouseMove() {
-    context.lineTo(mouse.x, mouse.y);
-    context.stroke();
-  }
-
-  canvas.addEventListener("mousedown", function() {
+  canvas.addEventListener("mousemove", function() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    const x1 = mouse.x * 2 - (x0 + x2) / 2;
+    const y1 = mouse.y * 2 - (y0 + y2) / 2;
     context.beginPath();
-    context.moveTo(mouse.x, mouse.y);
-    canvas.addEventListener("mousemove", onMouseMove, false);
-  });
-
-  canvas.addEventListener("mouseup", function() {
-    canvas.removeEventListener("mousemove", onMouseMove);
+    context.moveTo(x0, y0);
+    context.fillRect(x1, y1, 10, 10);
+    context.quadraticCurveTo(x1, y1, x2, y2);
+    context.stroke();
   });
 };
