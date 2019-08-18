7fd2206b306b96f1f6ac0785db36291533392f7c
Forjing <xiaodoubao1994@gmail.com>
2019-08-19 01:53:02 +0800

    投掷

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,13 @@ window.onload = function() {
   const mouse = captureMouse(canvas);
   const ball = new Ball(20, Math.random() * 0xffffff);
 
+  let isMouseDown = false;
+  let oldX;
+  let oldY;
+
+  let vx = 0;
+  let vy = 0;
+
   ball.x = canvas.width / 2;
   ball.y = canvas.height / 2;
 
@@ -23,6 +30,9 @@ window.onload = function() {
     if (containsPoint(ballRect, mouse.x, mouse.y)) {
       offsetx = mouse.x - ball.x;
       offsety = mouse.y - ball.y;
+      isMouseDown = true;
+      oldX = ball.x;
+      oldY = ball.y;
       canvas.addEventListener("mousemove", onMouseMove);
       canvas.addEventListener("mouseup", onMouseUp);
     }
@@ -34,14 +44,29 @@ window.onload = function() {
   }
 
   function onMouseUp() {
+    isMouseDown = false;
     canvas.removeEventListener("mousemove", onMouseMove);
     canvas.removeEventListener("mouseup", onMouseUp);
+  }
+
+  function trackVelocity() {
+    vx = ball.x - oldX;
+    vy = ball.y - oldY;
+    oldX = ball.x;
+    oldY = ball.y;
   }
 
   function drawFrame() {
     requestAnimationFrame(drawFrame);
 
     context.clearRect(0, 0, canvas.width, canvas.height);
+
+    if (isMouseDown) {
+      trackVelocity();
+    } else {
+      ball.x += vx;
+      ball.y += vy;
+    }
 
     ball.draw(context);
   }
