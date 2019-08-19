822a0c7d625af0b652ca1b78cffe963065ac26d9
liuqi <lq@hgobox.com>
2019-08-19 14:11:47 +0800

    缓动

--- src/index.ts
+++ src/index.ts
@@ -8,65 +8,18 @@ window.onload = function() {
   const context = canvas.getContext("2d");
   const mouse = captureMouse(canvas);
   const ball = new Ball(20, Math.random() * 0xffffff);
+  const easing = 0.05;
 
-  let isMouseDown = false;
-  let oldX;
-  let oldY;
-
-  let vx = 0;
-  let vy = 0;
-
-  ball.x = canvas.width / 2;
-  ball.y = canvas.height / 2;
-
-  ball.draw(context);
-
-  let offsetx = 0;
-  let offsety = 0;
-
-  canvas.addEventListener("mousedown", function() {
-    const ballRect = ball.getBounds();
-
-    if (containsPoint(ballRect, mouse.x, mouse.y)) {
-      offsetx = mouse.x - ball.x;
-      offsety = mouse.y - ball.y;
-      isMouseDown = true;
-      oldX = ball.x;
-      oldY = ball.y;
-      canvas.addEventListener("mousemove", onMouseMove);
-      canvas.addEventListener("mouseup", onMouseUp);
-    }
-  });
-
-  function onMouseMove() {
-    ball.x = mouse.x - offsetx;
-    ball.y = mouse.y - offsety;
-  }
-
-  function onMouseUp() {
-    isMouseDown = false;
-    canvas.removeEventListener("mousemove", onMouseMove);
-    canvas.removeEventListener("mouseup", onMouseUp);
-  }
-
-  function trackVelocity() {
-    vx = ball.x - oldX;
-    vy = ball.y - oldY;
-    oldX = ball.x;
-    oldY = ball.y;
-  }
+  let targetX = canvas.width / 2;
+  let targetY = canvas.height / 2;
 
   function drawFrame() {
     requestAnimationFrame(drawFrame);
 
     context.clearRect(0, 0, canvas.width, canvas.height);
 
-    if (isMouseDown) {
-      trackVelocity();
-    } else {
-      ball.x += vx;
-      ball.y += vy;
-    }
+    ball.x += (mouse.x - ball.x) * easing;
+    ball.y += (mouse.y - ball.y) * easing;
 
     ball.draw(context);
   }
